53d28d8241cdcf6b680d491863fa5a6bc25590b2
Alan <alanas.vaiciulis@gmail.com>
2020-08-29 15:10:07 +0300

    Add scenes modal to template

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -13,11 +13,38 @@
             <td>
                 {{ $project->title }}
             </td>
-            <th> <a href="#">Modal dialog su nuorodomis i atskira scena...</a> </th>
+            <td> 
+                <span class="text-success">({{ $project->scenosTotal }})</span>
+                <button type="button" data-toggle="modal" class="btn btn-primary" data-target="#showScenesModal-{{ $project->id }}">Perziureti</button>
+            </td>
             <td>
                 <button>Redaguoti</button>
             </td>
         </tr>
+        <!-- Modals -->
+        <div class="modal fade" id="showScenesModal-{{ $project->id }}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+            <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 style="text-align: center;" class="modal-title" id="myModalLabel">
+                        Projekto <strong> {{ $project->title }} </strong> scenos
+                    </h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                </div>
+                <div class="modal-body">
+                    <table>
+                    <th scope="col">#</th>
+                    <th scope="col">Pavadinimas</th>
+                    <th scope="col">Veiksmai</th>
+                    @foreach($project->scenes as $scene)
+                        <td>{{$scene->number}}</td>
+                        <td> <a href="#"> {{$scene->title}} </a> </td>
+                    @endforeach
+                    </table>
+                </div>
+            </div>
+            </div>
+        </div>
         @endforeach
     </table>
 @endsection
